937156f2b4e7c2147cafadc8a7d5ad8b5d7b159e
ayah.n <ayah.noor0213@gmail.com>
2020-03-01 16:23:26 -0500

    BasicNavigation

---
+++ src/test/java/com/automation/tests/day02/JumpToAnotherWindow.java
@@ -0,0 +1,23 @@
+package com.automation.tests.day02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JumpToAnotherWindow {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+
+        Thread.sleep(5000);
+
+        // every window has some id, this id called '"Window handle"
+        // based on window handle, we can switch in between windows
+        String windowHandle = driver.getWindowHandle();
+        System.out.println(windowHandle);
+    }
+}
